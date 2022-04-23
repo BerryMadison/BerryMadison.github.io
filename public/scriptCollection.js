dac2bec109d08acac44be6b6be17e66223710429
Berry Madison <58009872+BerryMadison@users.noreply.github.com>
2022-04-23 16:07:47 +0200

    Update scriptCollection.js

--- public/scriptCollection.js
+++ public/scriptCollection.js
@@ -42,30 +42,30 @@ function changeStyle(sheet) {
 	document.getElementById("pagestyle").setAttribute("href", sheet);
 	setCookie(sheet);
 	if(sheet == "stylesheet.css") {
-		document.getElementById("icon").setAttribute("href", "../images/skullmain.ico");
-		document.getElementById("myPic").setAttribute("src", "../images/me3.jpg");
+		document.getElementById("icon").setAttribute("href", "https://github.com/BerryMadison/BerryMadison.github.io/blob/main/images/skullmain.ico");
+		document.getElementById("myPic").setAttribute("src", "https://github.com/BerryMadison/BerryMadison.github.io/blob/main/images/me3.jpg");
 		document.getElementById("work").style.background = "none";
 		document.getElementById("about").style.background = "none";
 	} else if(sheet == "future.css") {
-		document.getElementById("icon").setAttribute("href", "../images/skullfuture.ico");
-		document.getElementById("myPic").setAttribute("src", "../images/steampunkme3.png");
+		document.getElementById("icon").setAttribute("href", "https://github.com/BerryMadison/BerryMadison.github.io/blob/main/images/skullfuture.ico");
+		document.getElementById("myPic").setAttribute("src", "https://github.com/BerryMadison/BerryMadison.github.io/blob/main/images/steampunkme3.png");
 		document.getElementById("content").classList.add("screen");
-		document.getElementById("work").style.background = "url('../images/futureneoncity.jpg') center no-repeat";
+		document.getElementById("work").style.background = "url('https://github.com/BerryMadison/BerryMadison.github.io/blob/main/images/futureneoncity.jpg') center no-repeat";
 		document.getElementById("work").style.backgroundSize = "cover";
-		document.getElementById("about").style.background = "url('../images/sci-fi-neon.jpg') center no-repeat";
+		document.getElementById("about").style.background = "url('https://github.com/BerryMadison/BerryMadison.github.io/blob/main/images/sci-fi-neon.jpg') center no-repeat";
 		document.getElementById("about").style.backgroundSize = "cover";
 		document.getElementById("content").classList.add("screen");
 		document.getElementsByClassName("content")[0].classList.add("screen");
 		document.getElementsByClassName("content")[1].classList.add("screen");
 	} else if(sheet == "space.css") {
-		document.getElementById("icon").setAttribute("href", "../images/skullspace.ico");
-		document.getElementById("myPic").setAttribute("src", "../images/DEDsec2.png");
-		document.getElementById("work").style.background = "url('../images/space4.jpg') center no-repeat";
+		document.getElementById("icon").setAttribute("href", "https://github.com/BerryMadison/BerryMadison.github.io/blob/main/images/skullspace.ico");
+		document.getElementById("myPic").setAttribute("src", "https://github.com/BerryMadison/BerryMadison.github.io/blob/main/images/DEDsec2.png");
+		document.getElementById("work").style.background = "url('https://github.com/BerryMadison/BerryMadison.github.io/blob/main/images/space4.jpg') center no-repeat";
 		document.getElementById("work").style.backgroundSize = "cover";
-		document.getElementById("about").style.background = "url('../images/space3.jpg') center no-repeat";
+		document.getElementById("about").style.background = "url('https://github.com/BerryMadison/BerryMadison.github.io/blob/main/images/space3.jpg') center no-repeat";
 		document.getElementById("about").style.backgroundSize = "cover";
 	} else {
-		document.getElementById("myPic").setAttribute("src", "../images/me2.jpg");
+		document.getElementById("myPic").setAttribute("src", "https://github.com/BerryMadison/BerryMadison.github.io/blob/main/images/me2.jpg");
 	}
 }
 
