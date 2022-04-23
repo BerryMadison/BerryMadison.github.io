52d9fd35dd16749b8f832c32b6b4038421160231
Berry Madison <58009872+BerryMadison@users.noreply.github.com>
2022-04-23 15:54:53 +0200

    Homepage

---
+++ public/scriptCollection.js
@@ -0,0 +1,78 @@
+const modalElement = document.getElementById("modalContent")
+	var myModal = new bootstrap.Modal(modalElement, {
+	  keyboard: false
+	})
+
+
+// setting, getting and check the value of the cookie
+function setCookie(cvalue) {
+	const d = new Date();
+	document.cookie ="style=" + cvalue + ";" + "SameSite=none";
+}
+
+function getCookie(cname) {
+	let name = cname + "=";
+	let ca = document.cookie.split(';');
+	for(let i = 0; i < ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+function checkCookie() {
+	let style = getCookie("style");
+	if (style != "") {
+		changeStyle(style);
+	} else {
+		style = document.getElementById("pagestyle").getAttribute("href");
+		console.log(style);
+		setCookie(style);
+    }
+}
+
+
+// Changes the stylesheet of the page
+function changeStyle(sheet) {
+	document.getElementById("pagestyle").setAttribute("href", sheet);
+	setCookie(sheet);
+	if(sheet == "stylesheet.css") {
+		document.getElementById("icon").setAttribute("href", "../images/skullmain.ico");
+		document.getElementById("myPic").setAttribute("src", "../images/me3.jpg");
+		document.getElementById("work").style.background = "none";
+		document.getElementById("about").style.background = "none";
+	} else if(sheet == "future.css") {
+		document.getElementById("icon").setAttribute("href", "../images/skullfuture.ico");
+		document.getElementById("myPic").setAttribute("src", "../images/steampunkme3.png");
+		document.getElementById("content").classList.add("screen");
+		document.getElementById("work").style.background = "url('../images/futureneoncity.jpg') center no-repeat";
+		document.getElementById("work").style.backgroundSize = "cover";
+		document.getElementById("about").style.background = "url('../images/sci-fi-neon.jpg') center no-repeat";
+		document.getElementById("about").style.backgroundSize = "cover";
+		document.getElementById("content").classList.add("screen");
+		document.getElementsByClassName("content")[0].classList.add("screen");
+		document.getElementsByClassName("content")[1].classList.add("screen");
+	} else if(sheet == "space.css") {
+		document.getElementById("icon").setAttribute("href", "../images/skullspace.ico");
+		document.getElementById("myPic").setAttribute("src", "../images/DEDsec2.png");
+		document.getElementById("work").style.background = "url('../images/space4.jpg') center no-repeat";
+		document.getElementById("work").style.backgroundSize = "cover";
+		document.getElementById("about").style.background = "url('../images/space3.jpg') center no-repeat";
+		document.getElementById("about").style.backgroundSize = "cover";
+	} else {
+		document.getElementById("myPic").setAttribute("src", "../images/me2.jpg");
+	}
+}
+
+// Show bigger Image on click
+function imagePopUp(e) {
+	var imgsrc = document.getElementsByClassName("projectImg")[e].getAttribute("src");
+	
+	document.getElementById("img").setAttribute("src", imgsrc);
+	myModal.show();
+}
